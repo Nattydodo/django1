39ea882805e198d87413bb830b79c06c147a6c97
Nattydodo <satirathai.n@gmail.com>
2024-09-16 15:59:49 +0700

    9/16/2024 Update

--- project2/about/views.py
+++ project2/about/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import About
 
 def about(request):
+  mymembers = About.objects.all().values()
   template = loader.get_template('aboutTem.html')
-  return HttpResponse(template.render())
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
